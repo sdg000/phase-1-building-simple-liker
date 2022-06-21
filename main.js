b9a4495e95201c7b31245197594ac31024c9936d
Seth David Gyimah <esec26@gmail.com>
2022-06-21 11:48:23 +0000

    building simple liker - revised

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
-
-const empty = document.getElementsByClassName('like-glyph')
+const empty = document.querySelectorAll('.like-glyph')
 
 //add eventListener to empty-heart class.
 for (element of empty){
-  element.addEventListener('click', mimicServerCall)
-    .then(function(response){
-      liked(response)
-    })
-
-    /*
-    .catch(function(error){
-      //call not liked function
-    })
-    */
-    
+  element.addEventListener('click', buttonHandle)   
 }
 
+function buttonHandle(event){
+  const heart = event.target
 
-function liked(event){
-  let button = event.target
-  if (button === EMPTY_HEART){
-    button.innerText = FULL_HEART
-    button.className = 'activated-heart' 
-    console.log(button)
-  }
+  mimicServerCall('url')
+
+  .then(function(){
+    if (heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.className = "activated-heart"
+    }
+    else {
+      heart.innerText = EMPTY_HEART
+      heart.className = ""
+
+    }
+  })
+
+  .catch(function(error){
+    const modal = document.getElementById("modal")
+    modal.className = ""
+    modal.innerText = error
+    setTimeout(modal.className = "hidden",3000)
+  })
 
 
 }
-
-// function unlike
-
 
 
 
